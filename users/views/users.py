06085754e7e09ef52783c5aab64202f92cc30a3a
Juan Manuel Acebal <juanmanuel.acebal@crowdbotics.com>
2022-08-27 10:46:26 -0300

    Add documentation to users endpoints

--- users/views/users.py
+++ users/views/users.py
@@ -2,11 +2,38 @@ from django.contrib.auth import get_user_model
 
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.viewsets import ModelViewSet
+from drf_spectacular.utils import extend_schema, extend_schema_view
 
 from users.serializers import UsersSerializer
 from garage.permissions import DeleteOnlyByAdmin
 from users.permissions import ManageOnlyByCurrentUser
 
+
+permission_admin_text = '\n\n__To use this endpoint, you must be an admin user.__'
+permission_user_text = '\n\n__To use this endpoint, you must be admin or the current user.__'
+
+
+@extend_schema_view(
+    list=extend_schema(
+        description=f'List all users. {permission_admin_text}'
+    ),
+    create=extend_schema(
+        description=f'Create a new user. {permission_admin_text}'
+    ),
+    retrieve=extend_schema(
+        description=f'Get a user by id. {permission_user_text}'
+    ),
+    update=extend_schema(
+        description=f'Update a user by id. {permission_user_text}'
+    ),
+    partial_update=extend_schema(
+        description=f'Partial update a user by id. {permission_user_text}'
+    ),
+    destroy=extend_schema(
+        description=f'Delete a user by id. {permission_admin_text}'
+    ),
+)
+@extend_schema(tags=['Users'])
 class UsersView(ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UsersSerializer
