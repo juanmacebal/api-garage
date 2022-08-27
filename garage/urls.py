588b615b805c20951290a9b7f64593ab2b757fa9
Juan Manuel Acebal <juanmanuel.acebal@crowdbotics.com>
2022-08-27 10:41:40 -0300

    Add Swagger

--- garage/urls.py
+++ garage/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1/users/', include('users.urls')),
+]
+
+# Swagger
+urlpatterns += [
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path("swagger/", SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
